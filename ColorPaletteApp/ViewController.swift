d50b1f83937da8a654adbe0d11b1d663430a7c92
AveryanovSS <mr.averyanov.s@gmail.com>
2021-03-28 22:39:13 +0300

    Completion of task 2.1

--- ColorPaletteApp/ViewController.swift
+++ ColorPaletteApp/ViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorRGBView: UIView!
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    @IBOutlet var redValueSlider: UISlider!
+    @IBOutlet var greenValueSlider: UISlider!
+    @IBOutlet var blueValueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorRGBView.layer.cornerRadius = 25
+        colorRGBView.backgroundColor = UIColor(
+            red: 0,
+            green: 0,
+            blue: 0,
+            alpha: 1
+        )
     }
-
-
+    
+    @IBAction func redValueSliderChange() {
+        redValueLabel.text = String(Int(redValueSlider.value))
+        colorRGBView.backgroundColor = UIColor(
+            red: CGFloat(redValueSlider.value / 255),
+            green: CGFloat(greenValueSlider.value / 255),
+            blue: CGFloat(blueValueSlider.value / 255),
+            alpha: 1
+        )
+    }
+    
+    @IBAction func greenValueSliderChange() {
+        greenValueLabel.text = String(Int(greenValueSlider.value))
+        colorRGBView.backgroundColor = UIColor(
+            red: CGFloat(redValueSlider.value / 255),
+            green: CGFloat(greenValueSlider.value / 255),
+            blue: CGFloat(blueValueSlider.value / 255),
+            alpha: 1
+        )
+    }
+    @IBAction func blueValueSliderChange() {
+        blueValueLabel.text = String(Int(blueValueSlider.value))
+        colorRGBView.backgroundColor = UIColor(
+            red: CGFloat(redValueSlider.value / 255),
+            green: CGFloat(greenValueSlider.value / 255),
+            blue: CGFloat(blueValueSlider.value / 255),
+            alpha: 1
+        )
+    }
+    
 }
 
